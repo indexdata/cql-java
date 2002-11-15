768c20f64b8b2ce463aab27cf844fad5865995fa
mike <mike>
2002-11-15 12:08:56 +0000

    Fix javadoc comments

--- src/org/z3950/zing/cql/CQLPrefix.java
+++ src/org/z3950/zing/cql/CQLPrefix.java
@@ -1,4 +1,4 @@
-// $Id: CQLPrefix.java,v 1.2 2002-11-15 12:02:29 mike Exp $
+// $Id: CQLPrefix.java,v 1.3 2002-11-15 12:08:56 mike Exp $
 
 package org.z3950.zing.cql;
 import java.lang.String;
@@ -6,20 +6,20 @@ import java.lang.String;
 /**
  * Represents a CQL prefix mapping from short name to long identifier.
  *
- * @version	$Id: CQLPrefix.java,v 1.2 2002-11-15 12:02:29 mike Exp $
+ * @version	$Id: CQLPrefix.java,v 1.3 2002-11-15 12:08:56 mike Exp $
  */
 public class CQLPrefix {
     /**
-     * The short name of the prefix mapping - that is, the prefix
+     * The short name of the prefix mapping.  That is, the prefix
      * itself, such as <TT>dc</TT>, as it might be used in a qualifier
      * like <TT>dc.title</TT>.
      */
     public String name;
 
     /**
-     * The full identifier name of the prefix mapping - that is, the prefix
-     * itself, such as <TT>dc</TT>, as it might be used in a qualifier
-     * like <TT>dc.title</TT>.
+     * The full identifier name of the prefix mapping.  That is,
+     * typically, a URI permanently allocated to a specific qualifier
+     * set, such as <TT>http://zthes.z3950.org/cql/<TT>.
      */
     public String identifier;
 
