561a15208b3be53c0ad3a1eb5524badfc377c478
mike <mike>
2002-11-29 16:42:54 +0000

    update documentation

--- src/org/z3950/zing/cql/UnknownPositionException.java
+++ src/org/z3950/zing/cql/UnknownPositionException.java
@@ -1,4 +1,4 @@
-// $Id: UnknownPositionException.java,v 1.1 2002-11-06 20:13:45 mike Exp $
+// $Id: UnknownPositionException.java,v 1.2 2002-11-29 16:42:54 mike Exp $
 
 package org.z3950.zing.cql;
 import java.lang.Exception;
@@ -7,13 +7,14 @@ import java.lang.Exception;
 /**
  * Exception indicating that a position was not recognised.
  * When rendering a tree out as PQF, each term is classified either as
- * <TT>anchored</TT> or <TT>unanchored</TT>, depending on whether it
- * begins with the word-anchoring meta-character <TT>^</TT>, and its
+ * <TT>any</TT>, <TT>first</TT>, <TT>last</TT> or
+ * <TT>firstAndLast</TT>, depending on whether it begins and/or ends
+ * with the word-anchoring meta-character <TT>^</TT>.  Its
  * classification is looked up as a <TT>position</TT> in the PQF
  * configuration.  If the position is not configured, we throw one of
  * these babies.
  *
- * @version $Id: UnknownPositionException.java,v 1.1 2002-11-06 20:13:45 mike Exp $
+ * @version $Id: UnknownPositionException.java,v 1.2 2002-11-29 16:42:54 mike Exp $
  */
 public class UnknownPositionException extends PQFTranslationException {
     /**
